2cc6b2375721f49e20d17706a92e55acdc55eaca
andreapmp <162078117+andreapmp@users.noreply.github.com>
2025-01-22 20:56:23 +0000

    NavBar changes

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,18 +1,20 @@
 import React from "react";
+import bravoLogo from "../../img/bravo-logo.png";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar bg-body-tertiary">
 			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
+				<a className="navbar-brand" href="#">
+					<img
+						src={bravoLogo}
+						alt="BravoLogo"
+						width={70}
+						height={90}
+					/>
+				</a>
+				<button class="btn btn-warning me-2" type="button">Get a Free Quote</button>
 			</div>
 		</nav>
 	);
